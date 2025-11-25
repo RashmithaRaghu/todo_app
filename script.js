38c1110d1cd87cda348c5b37ff429b27f3f9fa8c
Rashmitha R <rashmitha.rmenase@gmail.com>
2025-11-25 21:23:32 +0530

    updated js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
+const filterButtons = document.querySelectorAll("#filters button");
 
 input.addEventListener("keydown",function(e){
     if(e.key === "Enter" && input.value.trim()!== ""){
@@ -14,6 +15,7 @@ input.addEventListener("keydown",function(e){
 
         checkbox.addEventListener("change",function(){
             span.classList.toggle("completed");
+            li.classList.toggle("done");
         });
         li.appendChild(checkbox);
         li.appendChild(span);
@@ -21,3 +23,31 @@ input.addEventListener("keydown",function(e){
         input.value = "";
     }
 });
+
+filterButtons.forEach(btn=>{
+    btn.addEventListener("click",()=>{
+        const filter = btn.dataset.filter;
+        filterTasks(filter);
+    });
+});
+function filterTasks(filter){
+    const tasks = list.querySelectorAll("li");
+    tasks.forEach(task=>{
+        const isCompleted = task.classList.contains("done");
+        if(filter === "all"){
+            task.style.display = "flex";
+        }
+        else if(filter === "active" && isCompleted){
+            task.style.display = "none";
+        }
+        else if(filter === "active" && !isCompleted){
+            task.style.display = "flex";
+        }
+        else if(filter === "completed" && !isCompleted){
+            task.style.display = "none";
+        }
+        else if(filter === "completed" && isCompleted){
+            task.style.display = "flex";
+        }
+    });
+}
