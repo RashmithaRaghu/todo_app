0ed3e28917c5c717a33eef45f9f62615d28e3d90
Rashmitha R <rashmitha.rmenase@gmail.com>
2025-11-25 19:19:01 +0530

    updated js

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ const input = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
 
 input.addEventListener("keydown",function(e){
-    if(e.key === "Enter" && input.value.trim()!==""){
+    if(e.key === "Enter" && input.value.trim()!== ""){
         const li = document.createElement("li");
-        
 
-        li.addEventListener("click",function(){
-            li.classList.toggle("completed");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className = "todo-checkbox";
+
+        const span = document.createElement("span");
+        span.textContent = input.value;
+
+        checkbox.addEventListener("change",function(){
+            span.classList.toggle("completed");
         });
+        li.appendChild(checkbox);
+        li.appendChild(span);
         list.appendChild(li);
         input.value = "";
     }
-})
+});
