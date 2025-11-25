396203c01206f79abb427c81c63825e39d141938
Rashmitha R <rashmitha.rmenase@gmail.com>
2025-11-25 19:02:05 +0530

    updated js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const input = document.getElementById("todo-input");
-const list = document.getElementyId("todo-list");
+const list = document.getElementById("todo-list");
 
-input.addEventListener("keypress",function(e){
+input.addEventListener("keydown",function(e){
     if(e.key === "Enter" && input.value.trim()!==""){
         const li = document.createElement("li");
-        li.textContent = input.value;
+        
 
         li.addEventListener("click",function(){
             li.classList.toggle("completed");
